8cd9fd124d630047d78924aa70705f205d1c6732
bjohoxma <bjorn.hoxmark@schibsted.com>
2018-10-15 09:10:22 +0200

    updated readme and utils.js to comply with ESLint.

--- __tests__/utils.js
+++ __tests__/utils.js
@@ -201,17 +201,26 @@ test('.uriBuilder() - "base is empty" - should throw', () => {
 });
 
 test('.uriBuilder() - "base" has long path with <filename>.json - should replace <filename>.json file with "input"', () => {
-    const result = utils.uriBuilder('/podlet.html', 'http://localhost:7000/podlet/a/manifest.json');
+    const result = utils.uriBuilder(
+        '/podlet.html',
+        'http://localhost:7000/podlet/a/manifest.json',
+    );
     expect(result).toBe('http://localhost:7000/podlet/a/podlet.html');
 });
 
 test('.uriBuilder() - "base" has short path with <filename>.json - should replace <filename>.json file with "input"', () => {
-    const result = utils.uriBuilder('/podlet.html', 'http://localhost:7000/manifest.json');
+    const result = utils.uriBuilder(
+        '/podlet.html',
+        'http://localhost:7000/manifest.json',
+    );
     expect(result).toBe('http://localhost:7000/podlet.html');
 });
 
 test('.uriBuilder() - "base" has long path without <filename>.json - should append "input" to "base"', () => {
-    const result = utils.uriBuilder('/podlet.html', 'http://localhost:7000/podlet/a/');
+    const result = utils.uriBuilder(
+        '/podlet.html',
+        'http://localhost:7000/podlet/a/',
+    );
     expect(result).toBe('http://localhost:7000/podlet/a/podlet.html');
 });
 
@@ -221,7 +230,10 @@ test('.uriBuilder() - "base" has short path without <filename>.json - should app
 });
 
 test('.uriBuilder() - "input" does not begin with "/" - should replace <filename>.json file with "input"', () => {
-    const result = utils.uriBuilder('podlet.html', 'http://localhost:7000/podlet/a/manifest.json');
+    const result = utils.uriBuilder(
+        'podlet.html',
+        'http://localhost:7000/podlet/a/manifest.json',
+    );
     expect(result).toBe('http://localhost:7000/podlet/a/podlet.html');
 });
 
@@ -231,7 +243,11 @@ test('.uriBuilder() - "base" is without <filename>.json and does not end with "/
 });
 
 test('.uriBuilder() - "extra" is provided - should append "extra"', () => {
-    const result = utils.uriBuilder('/podlet', 'http://localhost:7000/foo/', '/a/b');
+    const result = utils.uriBuilder(
+        '/podlet',
+        'http://localhost:7000/foo/',
+        '/a/b',
+    );
     expect(result).toBe('http://localhost:7000/foo/podlet/a/b');
 });
 
@@ -244,7 +260,9 @@ test('.uriIsRelative() - "uri" is relative - should return "true"', () => {
 });
 
 test('.uriIsRelative() - "uri" is absolute - should return "false"', () => {
-    expect(utils.uriIsRelative('http://localhost:7000/manifest.json')).toBe(false);
+    expect(utils.uriIsRelative('http://localhost:7000/manifest.json')).toBe(
+        false,
+    );
 });
 
 /**
@@ -252,7 +270,11 @@ test('.uriIsRelative() - "uri" is absolute - should return "false"', () => {
  */
 
 test('.uriRelativeToAbsolute() - "input" is relative - should build absolute URI', () => {
-    const result = utils.uriRelativeToAbsolute('/podlet', 'http://localhost:7000/foo/', '/a/b');
+    const result = utils.uriRelativeToAbsolute(
+        '/podlet',
+        'http://localhost:7000/foo/',
+        '/a/b',
+    );
     expect(result).toBe('http://localhost:7000/foo/podlet/a/b');
 });
 
